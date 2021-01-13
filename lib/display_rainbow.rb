540c021198fa588d7bd2f2980827d13f11d6b20f
Keddie Valerio <keds266@gmail.com>
2021-01-13 03:22:22 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  puts "red:, orange:, yello:, green:, blue:, indigo:,violet"
+  puts ["red:, orange:, yello:, green:, blue:, indigo:,violet"]
 
 end
